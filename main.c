35eb1db46f8238a9f573471ed13244882526cbe2
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-09-16 07:52:15 -0700

    Fix formatting.
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

--- main.c
+++ main.c
@@ -3,34 +3,34 @@
 
 int main(int argc, char* argv[])
 {
-	if (SDL_Init(SDL_INIT_VIDEO) == 0) {
+    if (SDL_Init(SDL_INIT_VIDEO) == 0) {
 
-		SDL_Window* window = NULL;
-		SDL_Renderer* renderer = NULL;
+        SDL_Window* window = NULL;
+        SDL_Renderer* renderer = NULL;
 
-		if (SDL_CreateWindowAndRenderer((int)SCREEN_WIDTH, (int)SCREEN_HEIGHT, 0, &window, &renderer) == 0) {
-			SDL_bool done = SDL_FALSE;
-			while (!done) {
-				SDL_Event event;
+        if (SDL_CreateWindowAndRenderer((int)SCREEN_WIDTH, (int)SCREEN_HEIGHT, 0, &window, &renderer) == 0) {
+            SDL_bool done = SDL_FALSE;
+            while (!done) {
+                SDL_Event event;
 
-				while (SDL_PollEvent(&event)) {
-					switch (event.type) {
-					case SDL_QUIT:
-						done = SDL_TRUE;
-						break;
-					}
-				}
-			}
+                while (SDL_PollEvent(&event)) {
+                    switch (event.type) {
+                    case SDL_QUIT:
+                        done = SDL_TRUE;
+                        break;
+                    }
+                }
+            }
 
-			if (renderer) {
-				SDL_DestroyRenderer(renderer);
-			}
-			if (window) {
-				SDL_DestroyWindow(window);
-			}
-			SDL_Quit();
-		}
-	}
+            if (renderer) {
+                SDL_DestroyRenderer(renderer);
+            }
+            if (window) {
+                SDL_DestroyWindow(window);
+            }
+            SDL_Quit();
+        }
+    }
 
-	return 0;
+    return 0;
 }
