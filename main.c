e6c4cc7e1541c8ede32247b091b84e584bb832e3
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-09-17 10:04:35 -0700

    Use separate window and renderer creates.

--- main.c
+++ main.c
@@ -4,31 +4,39 @@
 int main(int argc, char* argv[])
 {
     if (SDL_Init(SDL_INIT_VIDEO) == 0) {
-
         SDL_Window* window = NULL;
         SDL_Renderer* renderer = NULL;
 
-        if (SDL_CreateWindowAndRenderer((int)SCREEN_WIDTH, (int)SCREEN_HEIGHT, 0, &window, &renderer) == 0) {
-            SDL_bool done = SDL_FALSE;
-            while (!done) {
-                SDL_Event event;
+        window = SDL_CreateWindow("", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, (int)SCREEN_WIDTH, (int)SCREEN_HEIGHT, 0);
 
-                while (SDL_PollEvent(&event)) {
-                    switch (event.type) {
-                    case SDL_QUIT:
-                        done = SDL_TRUE;
-                        break;
+        if (window != NULL) {
+
+            renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+
+            if (renderer != NULL) {
+
+                SDL_bool done = SDL_FALSE;
+
+                while (!done) {
+                    SDL_Event event;
+
+                    while (SDL_PollEvent(&event)) {
+                        switch (event.type) {
+                        case SDL_QUIT:
+                            done = SDL_TRUE;
+                            break;
+                        }
                     }
                 }
-            }
 
-            if (renderer) {
-                SDL_DestroyRenderer(renderer);
+                if (renderer) {
+                    SDL_DestroyRenderer(renderer);
+                }
+                if (window) {
+                    SDL_DestroyWindow(window);
+                }
+                SDL_Quit();
             }
-            if (window) {
-                SDL_DestroyWindow(window);
-            }
-            SDL_Quit();
         }
     }
 
